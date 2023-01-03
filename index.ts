236f64fba3be9b04072b2b16541054553e43df5a
John Bragg <remotezygote@gmail.com>
2023-01-03 12:52:57 -0800

    bump

--- index.ts
+++ index.ts
@@ -8,13 +8,13 @@ import responseTime from 'koa-response-time'
 import session from 'koa-generic-session'
 import jwt from 'koa-jwt'
 import jwksRsa from 'jwks-rsa'
-import bodyParser from 'koa-bodyparser'
+import body from 'koa-bodyparser'
 
 import { addGracefulShutdownHook, getHealthContextHandler, shutdown } from '@neurocode.io/k8s-graceful-shutdown'
 
 import logger from './logger'
 
-export { logger, bodyParser }
+export { logger, body }
 
 const app = new Koa()
 
