7287b64a85663c1a88f539b39fceb84c1d7f1fba
John Bragg <john@getarch.com>
2024-01-22 14:02:54 -0800

    fix: add default options for paginate

--- src/paginate/index.ts
+++ src/paginate/index.ts
@@ -5,4 +5,4 @@ import pagination from 'koa-pagination-v2'
 export const paginate = ({
   defaultLimit = process.env.DEFAULT_PAGE_LIMIT || 20,
   maximumLimit = process.env.MAX_PAGE_LIMIT || 50
-}) => pagination({ defaultLimit, maximumLimit })
+} = {}) => pagination({ defaultLimit, maximumLimit })
