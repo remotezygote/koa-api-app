106a41144ad80ba8593dd6df35a2a2c77fba6405
John Bragg <john@getarch.com>
2024-04-30 21:59:02 -0700

    extend a bit

--- src/security/index.ts
+++ src/security/index.ts
@@ -2,25 +2,32 @@ type Mechanism = string | Function
 
 const runMechanism = (mechanism: Mechanism, ctx: any, next: Function) => {
   if (typeof mechanism === 'function') {
-    return mechanism(ctx, next)
+    const output = mechanism(ctx, next)
+    return { output, success: output.success }
   } else if (typeof mechanism === 'string') {
-    return true
+    return {success: true, output: mechanism}
   } else if (typeof mechanism === 'object') {
-    return true
+    return {success: true, output: mechanism}
   } else {
-    return false
+    return {success: false, output: {}}
   }
 }
 export const security = (func: Function, mechanism: Mechanism) => {
   return async (ctx: any, next: Function) => {
     try {
-      if(await runMechanism(mechanism, ctx, next)) {
+      const { success, output } = await runMechanism(mechanism, ctx, next)
+      const { status, body, message } = output
+      if(success) {
+        ctx.state.security = output
         return await func(ctx, next)
       }
+      ctx.status = status || 403
+      ctx.body = {
+        message: message || 'Authorization not granted to resource.',
+        ...body
+      }
     } catch (err) {
-      ctx.status
-        ? ctx.status = 500
-        : ctx.status = 500
+      ctx.status = 500
       ctx.body = {
         message: (err as Error).message
       }
